f410e3833b4810fbb39e6584a4df544139747901
Alvin Yee <changchang.ye@hotmail.com>
2018-03-27 14:49:26 +0800

    Update test.py

--- codeGenerator/test.py
+++ codeGenerator/test.py
@@ -3,43 +3,54 @@ Created on 22th,March,2018
 
 @author: Alvin Ye
 '''
-from dbcParser.dbcParser import dbcParser
+import sys
+sys.path.append('../')
+sys.path.append('../../')
+
+from dbcParser import dbcParser
 from CanMsgLayoutDecoder.CanMsgLayoutDecoder import CanMsgLayoutDecoder
-from codeGenerator.CanMsgLayoutGenerator import c_bitField_Genertor
+from codeGenerator.CanMsgLayoutGenerator import c_signalBitField_Genertor
 
 if __name__ == "__main__":
-    import sys
     s = ''
     myParser = dbcParser()
     try:
         myParser.parseDbc(sys.argv[1])
-        print('INFO: dbc parsing succeed')
-        with open(sys.argv[2],'w') as f:
-            for msg in myParser._canNetwork.Nodes['ESCL'].NodeTxMsgs.values():
-
-                try:
-                    for m in CanMsgLayoutDecoder(msg).createLayoutMuliplexer().NodeTxMsgs.values():
-                        f.write("typedef struct _c_{0}_msgTypeTag\n".format(m.MsgName))
-                        f.write("{\n")
-                        c_bitField_Genertor(CanMsgLayoutDecoder(m).createLayout()).bitField_Genertor(f) 
-                        f.write("\n}")
-                        s += "\t_c_{0}_msgType {1};\n".format(m.MsgName,m.MsgName)    
-                        f.write("_c_{0}_msgType;\n".format(m.MsgName))
-                    f.write("typedef union _u_{0}_msgTypeTag\n".format(msg.MsgName))
-                    f.write("{\n")
-                    f.write(s)
-                    f.write("\n}")    
-                    f.write("_u_{0}_msgType;\n".format(msg.MsgName))                    
-                except :
-                    f.write("typedef struct _c_{0}_msgTypeTag\n".format(msg.MsgName))
-                    f.write("{\n")
-                    c_bitField_Genertor(CanMsgLayoutDecoder(msg).createLayout()).bitField_Genertor(f)    
-                    f.write("\n}")    
-                    f.write("_c_{0}_msgType;\n".format(msg.MsgName))
-            print("INFO: file generation succeed")
+        print('[INFO]: dbc parsing succeed')
+        with open(sys.argv[2],'w') as hFile:
+            with open(sys.argv[3],'w') as cFile:
+                for msg in myParser._canNetwork.Nodes['ESCL'].NodeTxMsgs.values():
+                    try:
+                        for m in CanMsgLayoutDecoder(msg).createLayoutMuliplexer().NodeTxMsgs.values():
+                            hFile.write("typedef struct _c_{0}_msgTypeTag\n".format(m.MsgName))
+                            hFile.write("{\n")
+                            signalGenerator = c_signalBitField_Genertor(CanMsgLayoutDecoder(m).createLayout())
+                            signalGenerator.bitField_Genertor(hFile) 
+                            hFile.write("\n}")
+                            s += "\t_c_{0}_msgType {1};\n".format(m.MsgName,m.MsgName)    
+                            hFile.write("_c_{0}_msgType;\n".format(m.MsgName))
+                            signalGenerator.signalAccessUnion_Genertor(msg,cFile) 
+                        hFile.write("typedef union _u_{0}_msgTypeTag\n".format(msg.MsgName))
+                        hFile.write("{\n")
+                        hFile.write(s)
+                        hFile.write("\n}")    
+                        hFile.write("_u_{0}_msgType;\n".format(msg.MsgName))   
+                        cFile.write("_u_{0}_msgType\t\t{1};\n".format(msg.MsgName,msg.MsgName))             
+                    except TypeError:
+                        hFile.write("typedef struct _c_{0}_msgTypeTag\n".format(msg.MsgName))
+                        hFile.write("{\n")
+                        signalGenerator = c_signalBitField_Genertor(CanMsgLayoutDecoder(msg).createLayout())
+                        signalGenerator.bitField_Genertor(hFile)    
+                        hFile.write("\n}")    
+                        hFile.write("_c_{0}_msgType;\n".format(msg.MsgName))
+                        cFile.write("_c_{0}_msgType\t\t{1};\n".format(msg.MsgName,msg.MsgName))
+                        signalGenerator.signalAccessStruct_Genertor(cFile) 
+                print("[INFO]: file generation succeed")
     except UnicodeDecodeError:
-        print("dbc decoding error, please convert dbc to utf-8 first")
+        print("[ERROR] dbc decoding error, please convert dbc to utf-8 first")
     except KeyError:
-        print("input the correct name of ESCL")
-    except:
-        print("ERROR: no dbc file designated")    
+        print("[ERROR] input the correct name of ESCL")
+    except FileNotFoundError:
+        print("[ERROR]: no dbc file designated")    
+    except IndexError:
+        print("[ERROR]: input or output directory is not properly given")
