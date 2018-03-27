dc833772f650bd212f41b38e3c9745b9d3108531
Alvin Yee <changchang.ye@hotmail.com>
2018-03-27 14:50:22 +0800

    Update test.py

--- dbcParser/test.py
+++ dbcParser/test.py
@@ -3,13 +3,21 @@ Created on 13th,March,2018
 
 @author: Alvin Ye
 '''
+import sys
+sys.path.append('../')
 import dbcParser
 if __name__ == "__main__":
-    import sys
     myParser = dbcParser.dbcParser()
     try:
         myParser.parseDbc(sys.argv[1])
-        myParser.iterateDbc()
-        print('INFO: dbc parsing succeed')
-    except:
-        print("ERROR: no dbc file designated")
+        with open(sys.argv[2],'w') as f:
+            myParser.iterateDbc(f)
+        print('[INFO]: dbc parsing succeed')
+    except UnicodeDecodeError:
+        print("[ERROR] dbc decoding error, please convert dbc to utf-8 first")
+    except KeyError:
+        print("[ERROR] input the correct name of ESCL")
+    except FileNotFoundError:
+        print("[ERROR]: no dbc file designated")
+    except IndexError:
+        print("[ERROR]: input or output directory is not properly given")
