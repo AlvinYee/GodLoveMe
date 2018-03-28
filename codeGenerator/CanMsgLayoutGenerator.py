b6cd188dcdad2f05b40585bce16874863e57eb6f
Alvin Yee <changchang.ye@hotmail.com>
2018-03-28 08:20:30 +0800

    Update CanMsgLayoutGenerator.py

--- codeGenerator/CanMsgLayoutGenerator.py
+++ codeGenerator/CanMsgLayoutGenerator.py
@@ -1,17 +1,17 @@
 '''
-Created on 13th,March,2018
+Created on 13th, March, 2018
 
 @author: Alvin Ye
 '''
 
 
-class Signalbit_field_generator:
+class SignalBitFieldGenerator:
 
 
     '''
     this class acts to slice a message into-byte long and create signal field inside every byte
     '''
-    def __init__(self,laidSignals):
+    def __init__(self, laidSignals):
         self._laidSignals = laidSignals
         self._byte_slicedSignals= [[]]
         self._syntax_type = 'uint8'
@@ -37,28 +37,28 @@ class Signalbit_field_generator:
                 self._byte_slicedSignals[self._byteIdx].append(signal)
         return self._byte_slicedSignals
     
-    def bit_field_generator(self,file):
+    def bit_field_generator(self, file):
         for byte_slice in self._byte_slicedSignals:
             for signal in byte_slice:
                 file.write(self.bit_field_struct_syntax(signal.signal_name, signal.signal_len))
                 
-    def bit_field_struct_syntax(self,signal_name,signalLenth):
+    def bit_field_struct_syntax(self, signal_name, signalLenth):
         return ('\t'+self._syntax_type+' '+signal_name+self._syntax_delimiter+str(signalLenth)+self._syntax_ending)
     
-    def signal_access_union_generator(self,unionMsg,file):
+    def signal_access_union_generator(self, unionMsg, file):
         for byte_slice in self._byte_slicedSignals:
             for signal in byte_slice:
                 if not signal.signal_name.startswith('unused'):
-                    file.write(self.signal_access_macro_syntax_union(signal.signal_name,unionMsg.msg_name, signal.signal_msg_carrier.msg_name))
+                    file.write(self.signal_access_macro_syntax_union(signal.signal_name, unionMsg.msg_name, signal.signal_msg_carrier.msg_name))
                     
-    def signal_access_struct_generator(self,file):
+    def signal_access_struct_generator(self, file):
         for byte_slice in self._byte_slicedSignals:
             for signal in byte_slice:
                 if not signal.signal_name.startswith('unused'):
                     file.write(self.signal_access_macro_syntax_struct(signal.signal_name, signal.signal_msg_carrier.msg_name))
                 
-    def signal_access_macro_syntax_union(self,signal_name,unionmsg_name,structmsg_name):
-        return '{0} b_{1}_b\t\t({2}.{3}.{4})\n'.format(self._syntax_define,signal_name,unionmsg_name,structmsg_name,signal_name)
+    def signal_access_macro_syntax_union(self, signal_name, unionmsg_name, structmsg_name):
+        return '{0} b_{1}_b\t\t({2}.{3}.{4})\n'.format(self._syntax_define, signal_name, unionmsg_name, structmsg_name, signal_name)
       
-    def signal_access_macro_syntax_struct(self,signal_name,structmsg_name):
-        return '{0} b_{1}_b\t\t({2}.{3})\n'.format(self._syntax_define,signal_name,structmsg_name,signal_name)
+    def signal_access_macro_syntax_struct(self, signal_name, structmsg_name):
+        return '{0} b_{1}_b\t\t({2}.{3})\n'.format(self._syntax_define, signal_name, structmsg_name, signal_name)
