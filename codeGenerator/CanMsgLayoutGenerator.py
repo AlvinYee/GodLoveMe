5c6bec86ff5645ce41dbd25bdd686164355f01b3
Alvin Yee <changchang.ye@hotmail.com>
2018-03-28 16:29:05 +0800

    Update CanMsgLayoutGenerator.py

--- codeGenerator/CanMsgLayoutGenerator.py
+++ codeGenerator/CanMsgLayoutGenerator.py
@@ -9,6 +9,7 @@ class SignalBitFieldSyntaxGenerator:
     '''
     this class acts to slice a message into-byte long and create signal field inside every byte
     '''
+
     def __init__(self, decoded_msg):
         self.decoded_msg = decoded_msg
         self._sliced_signals_by_byte = [[]]
@@ -24,7 +25,7 @@ class SignalBitFieldSyntaxGenerator:
 
         for byte_slice in self._slice_msg_by_byte():
             byte_slice.reverse()
-        
+
     def _slice_msg_by_byte(self):
         for signal in self.decoded_msg.msg_signals.values():
             self._total_len += signal.signal_len
@@ -35,23 +36,23 @@ class SignalBitFieldSyntaxGenerator:
                 self._byte_idx += 1
                 self._sliced_signals_by_byte[self._byte_idx].append(signal)
         return self._sliced_signals_by_byte
-    
+
     def bit_field_generator(self, file):
         for byte_slice in self._sliced_signals_by_byte:
             for signal in byte_slice:
                 file.write(self.bit_field_syntax(signal.signal_name, signal.signal_len))
-                
+
     def bit_field_syntax(self, signal_name, signal_len):
         return ('\t' + self._syntax_type + ' ' + signal_name + self._syntax_delimiter + str(
             signal_len) + self._syntax_ending)
-    
+
     def signal_union_generator(self, union_msg, file):
         for byte_slice in self._sliced_signals_by_byte:
             for signal in byte_slice:
                 if not signal.signal_name.startswith('unused'):
                     file.write(self.union_signal_macro_syntax(signal.signal_name, union_msg.msg_name,
                                                               signal.signal_msg_carrier.msg_name))
-                    
+
     def signal_struct_generator(self, file):
         for byte_slice in self._sliced_signals_by_byte:
             for signal in byte_slice:
@@ -61,7 +62,7 @@ class SignalBitFieldSyntaxGenerator:
     def union_signal_macro_syntax(self, signal_name, union_msg_name, struct_name):
         return '{0} b_{1}_b\t\t({2}.{3}.{4})\n'.format(self._syntax_define, signal_name, union_msg_name,
                                                        struct_name, signal_name)
-      
+
     def struct_signal_macro_syntax(self, signal_name, struct_msg_name):
         return '{0} b_{1}_b\t\t({2}.{3})\n'.format(self._syntax_define, signal_name, struct_msg_name, signal_name)
 
@@ -79,10 +80,13 @@ class SignalBitFieldSyntaxGenerator:
                     elif virtual_signals[0].signal_len <= 32:
                         self._syntax_data_type = 'uint32'
                     elif virtual_signals[0].signal_len <= 64:
-                        print('{0} - signal length is greater than CUP width, single access not supported'.format(virtual_signals[0].signal_name))
+                        print('{0} - signal length is greater than CUP width, single access not supported'.format(
+                            virtual_signals[0].signal_name))
                         continue
+                    else:
+                        pass
                     for signal in virtual_signals[1:]:
-                        s += '((({0}){1})<<{2}) | '.format(self._syntax_data_type, signal.signal_name, bit_shift)
+                        s += '((({0})b_{1}_b)<<{2}) | '.format(self._syntax_data_type, signal.signal_name, bit_shift)
                         bit_shift += signal.signal_len
                     else:
                         s = s.strip('| ')
@@ -97,10 +101,13 @@ class SignalBitFieldSyntaxGenerator:
                     elif virtual_signals[0].sinal_len <= 32:
                         self._syntax_data_type = 'uint32'
                     elif virtual_signals[0].signal_len <= 64:
-                        print('{0} - signal length is greater than CUP width, single access not supported'.format(virtual_signals[0].signal_name))
+                        print('{0} - signal length is greater than CUP width, single access not supported'.format(
+                            virtual_signals[0].signal_name))
                         continue
+                    else:
+                        pass
                     for signal in virtual_signals[:-1]:
-                        s += '((({0}){1})<<{2}) | '.format(self._syntax_data_type, signal.signal_name, bit_shift)
+                        s += '((({0})b_{1}_b)<<{2}) | '.format(self._syntax_data_type, signal.signal_name, bit_shift)
                         bit_shift += signal.signal_len
                     else:
                         s = s.strip('| ')
