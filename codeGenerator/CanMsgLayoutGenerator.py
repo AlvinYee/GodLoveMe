2bb4a01191081d7b0388d3246aa33dc02ea8ba88
Alvin Yee <changchang.ye@hotmail.com>
2018-03-28 14:45:46 +0800

    Update CanMsgLayoutGenerator.py

--- codeGenerator/CanMsgLayoutGenerator.py
+++ codeGenerator/CanMsgLayoutGenerator.py
@@ -5,60 +5,105 @@ Created on 13th, March, 2018
 '''
 
 
-class SignalBitFieldGenerator:
-
-
+class SignalBitFieldSyntaxGenerator:
     '''
     this class acts to slice a message into-byte long and create signal field inside every byte
     '''
-    def __init__(self, laidSignals):
-        self._laidSignals = laidSignals
-        self._byte_slicedSignals= [[]]
+    def __init__(self, decoded_msg):
+        self.decoded_msg = decoded_msg
+        self._sliced_signals_by_byte = [[]]
         self._syntax_type = 'uint8'
         self._syntax_struct = 'struct'
-        self._syntax_def    = 'typedef'
+        self._syntax_def = 'typedef'
         self._syntax_delimiter = ":"
         self._syntax_ending = ';\n'
-        self._totalLen = 0
-        self._byteIdx = 0
         self._syntax_define = '#define'
-        
-        for byte_slice in self.byte_slicer():
+        self._syntax_data_type = ''
+        self._total_len = 0
+        self._byte_idx = 0
+
+        for byte_slice in self._slice_msg_by_byte():
             byte_slice.reverse()
         
-    def byte_slicer(self):
-        for signal in self._laidSignals.values():
-            self._totalLen += signal.signal_len
-            if self._totalLen <= (self._byteIdx+1)*8:
-                self._byte_slicedSignals[self._byteIdx].append(signal)
+    def _slice_msg_by_byte(self):
+        for signal in self.decoded_msg.msg_signals.values():
+            self._total_len += signal.signal_len
+            if self._total_len <= (self._byte_idx + 1) * 8:
+                self._sliced_signals_by_byte[self._byte_idx].append(signal)
             else:
-                self._byte_slicedSignals.append([])
-                self._byteIdx +=1
-                self._byte_slicedSignals[self._byteIdx].append(signal)
-        return self._byte_slicedSignals
+                self._sliced_signals_by_byte.append([])
+                self._byte_idx += 1
+                self._sliced_signals_by_byte[self._byte_idx].append(signal)
+        return self._sliced_signals_by_byte
     
     def bit_field_generator(self, file):
-        for byte_slice in self._byte_slicedSignals:
+        for byte_slice in self._sliced_signals_by_byte:
             for signal in byte_slice:
-                file.write(self.bit_field_struct_syntax(signal.signal_name, signal.signal_len))
+                file.write(self.bit_field_syntax(signal.signal_name, signal.signal_len))
                 
-    def bit_field_struct_syntax(self, signal_name, signalLenth):
-        return ('\t'+self._syntax_type+' '+signal_name+self._syntax_delimiter+str(signalLenth)+self._syntax_ending)
+    def bit_field_syntax(self, signal_name, signal_len):
+        return ('\t' + self._syntax_type + ' ' + signal_name + self._syntax_delimiter + str(
+            signal_len) + self._syntax_ending)
     
-    def signal_access_union_generator(self, unionMsg, file):
-        for byte_slice in self._byte_slicedSignals:
+    def signal_union_generator(self, union_msg, file):
+        for byte_slice in self._sliced_signals_by_byte:
             for signal in byte_slice:
                 if not signal.signal_name.startswith('unused'):
-                    file.write(self.signal_access_macro_syntax_union(signal.signal_name, unionMsg.msg_name, signal.signal_msg_carrier.msg_name))
+                    file.write(self.union_signal_macro_syntax(signal.signal_name, union_msg.msg_name,
+                                                              signal.signal_msg_carrier.msg_name))
                     
-    def signal_access_struct_generator(self, file):
-        for byte_slice in self._byte_slicedSignals:
+    def signal_struct_generator(self, file):
+        for byte_slice in self._sliced_signals_by_byte:
             for signal in byte_slice:
                 if not signal.signal_name.startswith('unused'):
-                    file.write(self.signal_access_macro_syntax_struct(signal.signal_name, signal.signal_msg_carrier.msg_name))
-                
-    def signal_access_macro_syntax_union(self, signal_name, unionmsg_name, structmsg_name):
-        return '{0} b_{1}_b\t\t({2}.{3}.{4})\n'.format(self._syntax_define, signal_name, unionmsg_name, structmsg_name, signal_name)
+                    file.write(self.struct_signal_macro_syntax(signal.signal_name, signal.signal_msg_carrier.msg_name))
+
+    def union_signal_macro_syntax(self, signal_name, union_msg_name, struct_name):
+        return '{0} b_{1}_b\t\t({2}.{3}.{4})\n'.format(self._syntax_define, signal_name, union_msg_name,
+                                                       struct_name, signal_name)
       
-    def signal_access_macro_syntax_struct(self, signal_name, structmsg_name):
-        return '{0} b_{1}_b\t\t({2}.{3})\n'.format(self._syntax_define, signal_name, structmsg_name, signal_name)
+    def struct_signal_macro_syntax(self, signal_name, struct_msg_name):
+        return '{0} b_{1}_b\t\t({2}.{3})\n'.format(self._syntax_define, signal_name, struct_msg_name, signal_name)
+
+    def signal_access_macro_syntax(self, over_byte_bound_virtual_signals, file):
+        if over_byte_bound_virtual_signals:
+            for virtual_signals in over_byte_bound_virtual_signals:
+                s = ''
+                bit_shift = 0
+                if virtual_signals[0].signal_little_endian:
+                    # this is Intel
+                    if virtual_signals[0].signal_len <= 8:
+                        self._syntax_data_type = 'uint8'
+                    elif virtual_signals[0].signal_len <= 16:
+                        self._syntax_data_type = 'uint16'
+                    elif virtual_signals[0].signal_len <= 32:
+                        self._syntax_data_type = 'uint32'
+                    elif virtual_signals[0].signal_len <= 64:
+                        print('{0} - signal length is greater than CUP width, single access not supported'.format(virtual_signals[0].signal_name))
+                        continue
+                    for signal in virtual_signals[1:]:
+                        s += '((({0}){1})<<{2}) | '.format(self._syntax_data_type, signal.signal_name, bit_shift)
+                        bit_shift += signal.signal_len
+                    else:
+                        s = s.strip('| ')
+                        s = '(' + s + ')\n'
+                    s = '#define b_{0}_b\t'.format(virtual_signals[0].signal_name) + s
+                else:
+                    virtual_signals.reverse()
+                    if virtual_signals[-1].signal_len <= 8:
+                        self._syntax_data_type = 'uint8'
+                    elif virtual_signals[0].signal_len <= 16:
+                        self._syntax_data_type = 'uint16'
+                    elif virtual_signals[0].sinal_len <= 32:
+                        self._syntax_data_type = 'uint32'
+                    elif virtual_signals[0].signal_len <= 64:
+                        print('{0} - signal length is greater than CUP width, single access not supported'.format(virtual_signals[0].signal_name))
+                        continue
+                    for signal in virtual_signals[:-1]:
+                        s += '((({0}){1})<<{2}) | '.format(self._syntax_data_type, signal.signal_name, bit_shift)
+                        bit_shift += signal.signal_len
+                    else:
+                        s = s.strip('| ')
+                        s = '(' + s + ')\n'
+                    s = '#define b_{0}_b\t'.format(virtual_signals[0].signal_name) + s
+                file.write(s)
